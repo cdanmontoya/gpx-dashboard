2b0abeec6d9cd4fd86cfc0d6958e0fb9dfc5be88
cdanmontoya <carlos.danmontoya@gmail.com>
2023-10-09 09:41:49 -0500

    Version básica

--- dags/data_enrichment_dag.py
+++ dags/data_enrichment_dag.py
@@ -1,12 +1,32 @@
+import io
+from datetime import datetime
 from typing import Callable, Any
 
 import pandas as pd
-from airflow.decorators import task
+from airflow.decorators import dag, task
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.utils.trigger_rule import TriggerRule
 from geopy import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 
 geolocator = Nominatim(user_agent="geoapiExercises")
 geolocator = RateLimiter(geolocator.reverse, min_delay_seconds=1.2)
+s3_hook = S3Hook(aws_conn_id="minio_s3_connection")
+
+current_date = datetime.now()
+output_key = f'trips/year={current_date.year}/month={current_date.month}/day={current_date.day}/trips.parquet'
+
+
+@task()
+def read_dataframe(**kwargs):
+    bucket = kwargs["dag_run"].conf["bucket"]
+    key = kwargs["dag_run"].conf["key"]
+
+    s3_object = s3_hook.get_key(key, bucket)
+    df = pd.read_parquet(io.BytesIO(s3_object.get()['Body'].read()))
+
+    return df
 
 
 @task()
@@ -61,3 +81,45 @@ def merge_trips_with_geographic_attributes(trips_df: pd.DataFrame, geographic_da
     return (trips_df.merge(geographic_data_df, on=['trip', 'step'], how='left')
             .rename(columns=columns_to_rename)
             .drop(columns_to_drop, axis='columns'))
+
+
+@task()
+def store_enriched_df_to_stage(df: pd.DataFrame):
+    df.to_parquet('enriched.parquet')
+
+    s3_hook.load_file(
+        'enriched.parquet',
+        f'year={current_date.year}/month={current_date.month}/day={current_date.day}/enriched.parquet',
+        'stage',
+        True,
+        gzip=False
+    )
+
+
+trigger_feature_engineering = TriggerDagRunOperator(
+    task_id='trigger_feature_engineering',
+    trigger_dag_id='feature_engineering',
+    trigger_rule=TriggerRule.ALL_SUCCESS,
+    conf={
+        "bucket": 'stage',
+        "key": output_key,
+        "url": f's3://stage/{output_key}',
+    }
+)
+
+
+@dag(
+    dag_id="data_enrichment",
+    start_date=datetime.now(),
+    schedule=None
+)
+def data_enrichment():
+    data = read_dataframe()
+
+    first_last_df = get_first_and_last_trip_points(data)
+    geographic_data_df = add_geographic_attributes(first_last_df, geocoder=geolocator)
+    enriched_df = merge_trips_with_geographic_attributes(data, geographic_data_df)
+    store_enriched_df_to_stage(enriched_df) >> trigger_feature_engineering
+
+
+data_enrichment()
--- dags/feature_engineering_dag.py
+++ dags/feature_engineering_dag.py
@@ -96,23 +96,71 @@ def merge_features(enriched_df: pd.DataFrame,
 
 
 @task()
-def store_to_stage(df: pd.DataFrame):
-    df.to_parquet('features.parquet')
+def store_to_datalake(stage: str, name: str, df: pd.DataFrame):
+    df.to_parquet(f'{name}.parquet')
 
     current_date = datetime.now()
 
     s3_hook.load_file(
-        'features.parquet',
-        f'year={current_date.year}/month={current_date.month}/day={current_date.day}/features.parquet',
-        'stage',
+        f'{name}.parquet',
+        f'year={current_date.year}/month={current_date.month}/day={current_date.day}/{name}.parquet',
+        stage,
         True,
         gzip=False
     )
 
 
 @task()
-def store_to_db(df: pd.DataFrame):
-    df.to_sql('feature_data', engine)
+def store_to_db(name: str, df: pd.DataFrame):
+    df.to_sql(name, engine, if_exists='replace')
+
+
+@task()
+def compact_data(df: pd.DataFrame) -> pd.DataFrame:
+    origin_rename = {
+        'time': 'origin_time',
+        'country': 'origin_country',
+        'state': 'origin_state',
+        'city': 'origin_city',
+        'suburb': 'origin_suburb',
+        'neighbourhood': 'origin_neighbourhood',
+        'postcode': 'origin_postcode',
+        'latitude': 'origin_latitude',
+        'longitude': 'origin_longitude',
+        'elevation': 'origin_elevation',
+        'weekday': 'origin_weekday',
+        'hour': 'origin_hour',
+    }
+
+    destination_rename = {
+        'time': 'destination_time',
+        'country': 'destination_country',
+        'state': 'destination_state',
+        'city': 'destination_city',
+        'suburb': 'destination_suburb',
+        'neighbourhood': 'destination_neighbourhood',
+        'postcode': 'destination_postcode',
+        'latitude': 'destination_latitude',
+        'longitude': 'destination_longitude',
+        'elevation': 'destination_elevation',
+        'weekday': 'destination_weekday',
+        'hour': 'destination_hour',
+    }
+
+    origin = (df.groupby('trip', as_index=False)
+              .first()
+              .rename(columns=origin_rename)
+              .drop(columns=['step', 'elapsed_time', 'distance', 'average_speed']))
+
+    destination = (df.groupby('trip', as_index=False)
+                   .tail(1)
+                   .rename(columns=destination_rename)
+                   .drop(columns=['step', 'elapsed_time', 'distance', 'average_speed', ]))
+
+    return (df.merge(origin, on=['trip'], how='inner')
+            .merge(destination, on=['trip'], how='inner')
+            .drop(columns=list(origin_rename.keys()))
+            .drop_duplicates(subset=['trip']))
 
 
 @dag(
@@ -126,8 +174,8 @@ def feature_engineering():
     distance_df = get_distance(data)
     weekday_df = get_weekday_and_hour(data)
     feature_df = merge_features(data, elapsed_time_df, distance_df, weekday_df)
-    store_to_stage(feature_df)
-    store_to_db(feature_df)
+    store_to_datalake('stage', 'features', feature_df)
+    store_to_db('feature_data', feature_df)
 
 
 feature_engineering()
